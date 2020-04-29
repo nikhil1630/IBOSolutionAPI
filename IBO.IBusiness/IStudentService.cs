6c46980eebeab081d08dd56c665b22504def7df5
nikhil.dhore92@gmail.com <nikhil.dhore92@gmail.com>
2020-04-29 16:33:40 +0530

    commit Setup

---
+++ IBO.IBusiness/IStudentService.cs
@@ -0,0 +1,17 @@
+﻿using IBO.Business.DTOs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBO.IBusiness
+{
+    public interface IStudentService
+    {
+        Task<StudentDTOs> GetAllStudentDetails();
+        Task<StudentDTOs> GetAllStudentName();
+        Task<StudentDTOs> GetStudentByID(int id);
+       
+    }
+}
---
+++ IBO.IRepository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IBO.IRepository
+{
+    public class Class1
+    {
+    }
+}
